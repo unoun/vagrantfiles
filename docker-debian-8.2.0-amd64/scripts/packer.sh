a7ca1c60da95cf9abde84cdca83a9f924e16fb73
unoun <ounouno+github@gmail.com>
2015-09-09 08:45:17 +0900

    Update to Packer 0.8.6

--- docker-debian-8.2.0-amd64/scripts/packer.sh
+++ docker-debian-8.2.0-amd64/scripts/packer.sh
@@ -4,21 +4,23 @@ set -e
 
 # install packer
 
+packer_version="0.8.6"
+
 apt-get install unzip
 
 rm -rf /usr/local/packer
 mkdir /usr/local/packer
 chmod 2775 /usr/local/packer
 cd /tmp
-if [ -f /tmp/packer_0.7.5_linux_amd64.zip ]; then
-  sum="$(md5sum /tmp/packer_0.7.5_linux_amd64.zip | cut -f1 -d' ')"
+if [ -f /tmp/packer_${packer_version}_linux_amd64.zip ]; then
+  sum="$(sha256sum /tmp/packer_${packer_version}_linux_amd64.zip | cut -f1 -d' ')"
 else
   sum=""
 fi
-if [ "$sum" != "f343d709b84db494e8d6ec38259aa4a6" ]; then
-  wget --quiet https://dl.bintray.com/mitchellh/packer/packer_0.7.5_linux_amd64.zip
+if [ "$sum" != "2f1ca794e51de831ace30792ab0886aca516bf6b407f6027e816ba7ca79703b5" ]; then
+  wget --quiet https://dl.bintray.com/mitchellh/packer/packer_${packer_version}_linux_amd64.zip
 fi
-cd /usr/local/packer && unzip /tmp/packer_0.7.5_linux_amd64.zip
+cd /usr/local/packer && unzip /tmp/packer_${packer_version}_linux_amd64.zip
 
 echo 'export PATH=$PATH:/usr/local/packer' >/etc/profile.d/20packer.sh
 grep secure_path /etc/sudoers | sed -e 's%"$%:/usr/local/packer"%' >/etc/sudoers.d/packer
