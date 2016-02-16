d1d11f76ec3877238b5cca389d8e46fafe52fb70
unoun <ounouno+github@gmail.com>
2016-02-17 08:26:39 +0900

    Change docker storage driver from aufs to overlay

--- docker-debian-8.3.0-amd64/scripts/provision.sh
+++ docker-debian-8.3.0-amd64/scripts/provision.sh
@@ -15,8 +15,13 @@ Pin-Priority: 90
 _EOF
 
 apt-get update
-apt-get -y install -t testing lvm2
+apt-get -y install -t testing lvm2 linux-image-4.3.0-1-amd64 linux-headers-4.3.0-1-amd64
 apt-get -y install docker.io
+
+dkms autoinstall -k 4.3.0-1-amd64
+
+grep -q '^DOCKER_OPTS=' /etc/default/docker \
+|| sed -i '/DOCKER_OPTS=/aDOCKER_OPTS="--storage-driver=overlay"' /etc/default/docker
 
 grep -q 'cgroup_enable=memory swapaccount=1' /etc/default/grub \
 || sed -i 's/\(GRUB_CMDLINE_LINUX_DEFAULT="\)/\1cgroup_enable=memory swapaccount=1 /' /etc/default/grub
